d69df9a34e296e175deca680f3ba354e5e66c519
Fouad Bahrani Pour <49376087+bahranipour@users.noreply.github.com>
2025-02-14 21:09:01 +0330

    Add files via upload

---
+++ tentacles.py
@@ -0,0 +1,6 @@
+tentacles = input('Do you have tentacles? (n/y): ')
+
+if tentacles == 'y':
+    print('I never knew octopuses could type!')
+else:
+    print('Greetings, human!')
---
+++ weather.py
@@ -0,0 +1,8 @@
+weather = input('What is the forecast for today? (rain/snow/sun): ')
+
+if weather == 'rain':
+    print('Remember your umbrella!')
+elif weather == 'snow':
+    print('Remember your wooly gloves!')
+else:
+    print('Remember your sunglasses!')
